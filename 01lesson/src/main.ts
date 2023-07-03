e6099f7403025aac2b2c3cc2d5e614e3d41d01a7
salamanderctesiphon <swbrookshire@gmail.com>
2023-07-03 14:05:55 -0500

    hi

--- 01lesson/src/main.ts
+++ 01lesson/src/main.ts
@@ -1,9 +1,41 @@
-let myName: string = 'Dave';
-let meaningOfLife: number;
-let isLoading: boolean;
-let album: any;
+let stringArray = ['one', 'heu', 'Dave'];
+let guitar = ['Strat', 'Les Paul', 5150];
+let mixedData = [ 'EVH', 1984, true];
 
-myName = 'bob';
-meaningOfLife = 42;
-isLoading = true;
-album = true;
+stringArray[0] = 'John';
+
+stringArray.push('her');
+
+guitar[0] = 1984;
+
+
+let test = [];
+
+let bands: string[] = []; 
+
+//tuple:
+
+let myTuple: [string, number, boolean] = ['Dave', 42, true];
+
+
+//objects
+
+
+let myObj: object = {}
+
+const exampleObj = {
+  prop1: 'Dave',
+  prop2: true
+}
+
+type Guitarist = {
+  name: string,
+  active: boolean,
+  albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+  name: 'Eddie',
+  active: false,
+  albums: [1984, 5150, 'OU812']
+}
