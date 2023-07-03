56754ee8bbe7d8b5fe34241650ad06997a2e2ece
salamanderctesiphon <swbrookshire@gmail.com>
2023-07-03 14:05:29 -0500

    git push

--- 01lesson/build/js/main.js
+++ 01lesson/build/js/main.js
@@ -1,2 +1,22 @@
 "use strict";
-let myName = 'Dave';
+let stringArray = ['one', 'heu', 'Dave'];
+let guitar = ['Strat', 'Les Paul', 5150];
+let mixedData = ['EVH', 1984, true];
+stringArray[0] = 'John';
+stringArray.push('her');
+guitar[0] = 1984;
+let test = [];
+let bands = [];
+//tuple:
+let myTuple = ['Dave', 42, true];
+//objects
+let myObj = {};
+const exampleObj = {
+    prop1: 'Dave',
+    prop2: true
+};
+let evh = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812']
+};
