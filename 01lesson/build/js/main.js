d00346236ebe05f07c8aed105e296d01c872b335
salamanderctesiphon <swbrookshire@gmail.com>
2023-07-05 11:23:39 -0500

    wow just wow

--- 01lesson/build/js/main.js
+++ 01lesson/build/js/main.js
@@ -1,1 +1,10 @@
 "use strict";
+// Index Signatures
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions['Pizza']);
+let prop = "Pizza";
---
+++ 01lesson/src/main.ts
@@ -0,0 +1,18 @@
+// Index Signatures
+
+interface TransactionObj {
+  Pizza: number,
+  Books: number,
+  Job: number
+}
+
+const todaysTransactions: TransactionObj = {
+  Pizza: -10,
+  Books: -5, 
+  Job: 50
+}
+
+console.log(todaysTransactions.Pizza)
+console.log(todaysTransactions['Pizza'])
+
+let prop: string = "Pizza"
